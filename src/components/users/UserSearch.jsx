6fc06755f5af7cc05e3d7b57b1c8c3243c64f94d
Julian Grisales <jgrisales@gmail.com>
2022-01-04 11:58:37 +0100

    Search Users

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -3,7 +3,7 @@ import GithubContext from "../../context/github/GithubContext"
 
 const UserSearch = () => {
   const [text, setText] = useState("")
-  const { users } = useContext(GithubContext)
+  const { users, searchUsers } = useContext(GithubContext)
 
   const handleChange = e => setText(e.target.value)
   const handleSubmit = e => {
@@ -12,7 +12,7 @@ const UserSearch = () => {
     if (text === "") {
       alert("Please enter something")
     } else {
-      // TODO search users
+      searchUsers(text)
       setText("")
     }
   }
