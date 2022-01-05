2951132f78c265ce69e93e86d9449546d0cea183
Julian Grisales <jgrisales@gmail.com>
2022-01-05 19:42:16 +0100

    Repo Items (code of brad)

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -1,15 +1,24 @@
-import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from "react-icons/fa"
-import PropTypes from "prop-types"
+import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from 'react-icons/fa'
 
-const RepoItem = ({ repo }) => {
-  const { name, description, html_url, forks, open_issues, watchers_count, stargazers_count } = repo
+import PropTypes from 'prop-types'
+
+function RepoItem({ repo }) {
+  const {
+    name,
+    description,
+    html_url,
+    forks,
+    open_issues,
+    watchers_count,
+    stargazers_count,
+  } = repo
 
   return (
-    <div className="mb-2 rounded-md card bg-gray-800 hover:bg-gray-900">
-      <div className="card-body">
-        <h3 className="mb-2 text-xl font-semibold">
+    <div className='mb-2 rounded-md card bg-gray-800 hover:bg-gray-900'>
+      <div className='card-body'>
+        <h3 className='mb-2 text-xl font-semibold'>
           <a href={html_url}>
-            <FaLink className="inline mr-1" /> {name}
+            <FaLink className='inline mr-1' /> {name}
           </a>
         </h3>
       </div>
@@ -18,7 +27,7 @@ const RepoItem = ({ repo }) => {
 }
 
 RepoItem.propTypes = {
-  repo: PropTypes.object.isRequired
+  repo: PropTypes.object.isRequired,
 }
 
 export default RepoItem
