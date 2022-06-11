8c79505960f7edfb5800bef41fd557ba9643f6ac
Julian Grisales <jgrisales@gmail.com>
2022-06-11 09:31:02 +0200

    Rolling back the test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import UserSearch from "../components/users/UserSearch"
 const Home = () => {
   return (
     <div>
-      <h1>Hallo change send to GitHub</h1>
       <UserSearch />
       <UserResults />
     </div>
